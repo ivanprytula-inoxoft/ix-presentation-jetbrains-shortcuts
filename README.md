afb45add20d71acdbfb60e02fd009bf81be6ab93
ivanprytula <johnnyforester@gmail.com>
2020-03-20 18:16:35 +0200

    Add Tip #1

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## CREDITS
-Copyright: 2018-2019 Paul Everitt, PyCharm Developer Advocate at JetBrains  
+Copyright (c) 2018-2019 Paul Everitt, PyCharm Developer Advocate at JetBrains, @paulweveritt  
 [GitHub](https://github.com/pauleveritt/42-workshop)  
 [JetBrains PyCharm related page](https://www.jetbrains.com/pycharm/guide/playlists/42/)
+
+----
+### Python is cool =)
---
+++ e01_find_action.py
@@ -0,0 +1,31 @@
+"""
+01: Find Action
+
+Skip memorizing keyboard shortcuts and speed search for actions instead.
+
+? I want to find all instances of the string `Python is cool`
+
+? What is the Find in Path shortcut? Who knows?
+
+1. Use Find Action (Shift-Ctrl-A Win/Linux, Shift-Cmd-A macOS)
+
+2. Speed type "fi pa", note shortcut, select
+
+3. Search 'Pyt' to find 'Python is cool'
+
+- Also for preferences, e.g. `python import`
+
+Repo: https://github.com/pauleveritt/42-workshop
+Playlist: https://www.jetbrains.com/pycharm/guide/playlists/42/
+"""
+
+import antigravity
+
+
+def e1_find_action():
+    print('Use "Find Action" (Shift-Ctrl-A Win/Linux, Shift-Cmd-A macOS)')
+    print(antigravity)
+
+
+if __name__ == '__main__':
+    e1_find_action()
